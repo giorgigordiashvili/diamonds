03be16474018312369d9dac8a7e5ea070152a073
Giorgi Gordiashvili <giorgigordiashvili@mackbooks-MacBook-Pro.local>
2025-04-22 12:23:27 +0400

    updates

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,62 +1,23 @@
 import LanguageSwitcher from '@/components/LanguageSwitcher';
-import Image from 'next/image';
+import { getDictionary } from '@/get-dictionary';
+import { Locale } from '@/i18n-config';
 import styles from '../page.module.css';
-import { getDictionary } from './dictionaries';
-import { locales } from './layout';
 
-// Generate static routes for supported locales
-export async function generateStaticParams() {
-  return locales.map((lang) => ({ lang }));
-}
-
-export default async function Home({ params }: { params: { lang: string } }) {
+export default async function IndexPage(props: { params: Promise<{ lang: Locale }> }) {
   // Load dictionary based on the current language
-  const dict = await getDictionary(params.lang);
+  const { lang } = await props.params;
 
+  const dictionary = await getDictionary(lang);
   return (
     <div className={styles.page}>
       <LanguageSwitcher />
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
         <ol>
           <li>
-            {dict.home.description} <code>src/app/[lang]/page.tsx</code>.
+            {dictionary.home.description} <code>src/app/[lang]/page.tsx</code>.
           </li>
-          <li>{dict.home.save_changes}</li>
+          <li>{dictionary.home.save_changes}</li>
         </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            {dict.home.deploy_now}
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            {dict.home.read_docs}
-          </a>
-        </div>
       </main>
       <footer className={styles.footer}>
         <a
@@ -64,24 +25,21 @@ export default async function Home({ params }: { params: { lang: string } }) {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image aria-hidden src="/file.svg" alt="File icon" width={16} height={16} />
-          {dict.home.learn}
+          {dictionary.home.learn}
         </a>
         <a
           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image aria-hidden src="/window.svg" alt="Window icon" width={16} height={16} />
-          {dict.home.examples}
+          {dictionary.home.examples}
         </a>
         <a
           href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image aria-hidden src="/globe.svg" alt="Globe icon" width={16} height={16} />
-          {dict.home.go_to_nextjs}
+          {dictionary.home.go_to_nextjs}
         </a>
       </footer>
     </div>
