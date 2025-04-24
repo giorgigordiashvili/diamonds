2b038a8321068d63fa1ee1b83de1d212355bd5fe
Maariami <mariam8bidzinashvili@gmail.com>
2025-04-25 01:17:46 +0400

    filters ogond responsive

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,13 +1,13 @@
 'use client';
 import LanguageSwitcher from '@/components/LanguageSwitcher';
 import styles from '../page.module.css';
-import DiamondsSection from '@/components/DiamondsSection';
+import DiamondsFilter from '@/components/DiamondsFilter';
 
 export default function IndexPage() {
   return (
     <div className={styles.page}>
       <LanguageSwitcher />
-      <DiamondsSection />
+      <DiamondsFilter />
     </div>
   );
 }
